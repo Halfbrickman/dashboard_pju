3a046bffa6f34c1a890f284400ecfe9af96827d6
tgh-01 <teguhegifirdaus@gmail.com>
2025-09-04 13:21:11 +0700

    update keterangan dashboard dinamis

--- app/Views/Template/assetDashboard.php
+++ app/Views/Template/assetDashboard.php
@@ -30,7 +30,7 @@
         koordinatData.forEach(function(item) {
             // Definisikan ikon SVG kustom dengan warna dinamis
             var flaticonSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="${item.warna}" d="M128 252.6C128 148.4 214 64 320 64C426 64 512 148.4 512 252.6C512 371.9 391.8 514.9 341.6 569.4C329.8 582.2 310.1 582.2 298.3 569.4C248.1 514.9 127.9 371.9 127.9 252.6zM320 320C355.3 320 384 291.3 384 256C384 220.7 355.3 192 320 192C284.7 192 256 220.7 256 256C256 291.3 284.7 320 320 320z"/></svg>`;
-            
+
             var customIcon = L.divIcon({
                 className: 'custom-pin', // Class CSS jika ada
                 html: flaticonSVG,
@@ -56,15 +56,15 @@
                 </button>
             `;
             marker.bindPopup(popupContent);
-            
+
             // Masukkan marker yang sudah jadi ke dalam array
-            markerList.push(marker); 
+            markerList.push(marker);
         });
 
         // 5. Tambahkan semua marker sekaligus ke grup cluster
-        markers.addLayers(markerList); 
+        markers.addLayers(markerList);
         map.addLayer(markers);
-        
+
         // Atur view peta agar semua marker terlihat
         if (markerList.length > 0) {
             map.fitBounds(markers.getBounds());
@@ -74,27 +74,44 @@
         document.addEventListener('click', function(e) {
             if (e.target && e.target.classList.contains('detail-btn')) {
                 var koordinatId = e.target.getAttribute('data-id');
-                var data = koordinatMap[koordinatId]; // Pencarian data super cepat
+                var data = koordinatMap[koordinatId]; // Ambil data lengkap dari map
 
                 if (data) {
                     var modalBody = document.getElementById('detail-content');
-                    var contentHtml = `
-                        <table class="table table-striped table-bordered">
-                            <tbody>
-                                <tr><th style="width: 30%;">ID Koordinat</th><td>${data.id_koordinat}</td></tr>
-                                <tr><th>Sumber Data</th><td>${data.nama_sumber}</td></tr>
-                                <tr><th>Kota/Kabupaten</th><td>${data.nama_kotakab || 'N/A'}</td></tr>
-                                <tr><th>Kecamatan</th><td>${data.nama_kec || 'N/A'}</td></tr>
-                                <tr><th>Kelurahan</th><td>${data.nama_kel || 'N/A'}</td></tr>
-                                <tr><th>Latitude</th><td>${data.latitude}</td></tr>
-                                <tr><th>Longitude</th><td>${data.longitude}</td></tr>
-                                <tr><th>Nomor Gardu</th><td>${data.nomor_gardu || 'N/A'}</td></tr>
-                                <tr><th>Daya PJU</th><td>${data.daya_pju || 'N/A'}</td></tr>
-                                <tr><th>Kondisi PJU</th><td>${data.kondisi_pju || 'N/A'}</td></tr>
-                            </tbody>
-                        </table>
-                    `;
-                    modalBody.innerHTML = contentHtml;
+
+                    // 1. Buat bagian HTML yang umum untuk semua sumber data
+                    let commonHtml = `
+                <tr><th style="width: 30%;">ID Koordinat</th><td>${data.id_koordinat}</td></tr>
+                <tr><th>Sumber Data</th><td>${data.nama_sumber}</td></tr>
+                <tr><th>Kota/Kabupaten</th><td>${data.nama_kotakab || 'N/A'}</td></tr>
+                <tr><th>Kecamatan</th><td>${data.nama_kec || 'N/A'}</td></tr>
+                <tr><th>Kelurahan</th><td>${data.nama_kel || 'N/A'}</td></tr>
+                <tr><th>Latitude</th><td>${data.latitude}</td></tr>
+                <tr><th>Longitude</th><td>${data.longitude}</td></tr>
+            `;
+
+                    // **[MODIFIKASI UTAMA DIMULAI DI SINI]**
+                    // 2. Buat variabel untuk menampung HTML spesifik secara dinamis
+                    let specificHtml = '';
+
+                    // 3. Lakukan looping pada array 'keterangan_tambahan' yang dikirim dari controller
+                    if (data.keterangan_tambahan && data.keterangan_tambahan.length > 0) {
+                        data.keterangan_tambahan.forEach(function(item) {
+                            // item akan berisi: { jdl_keterangan: "Labelnya", isi_keterangan: "Nilainya" }
+                            specificHtml += `<tr><th>${item.jdl_keterangan}</th><td>${item.isi_keterangan || 'N/A'}</td></tr>`;
+                        });
+                    }
+                    // **[MODIFIKASI SELESAI]**
+
+                    // 4. Gabungkan HTML umum dan spesifik, lalu tampilkan di modal
+                    modalBody.innerHTML = `
+                <table class="table table-striped table-bordered">
+                    <tbody>
+                        ${commonHtml}
+                        ${specificHtml}
+                    </tbody>
+                </table>
+            `;
                 }
             }
         });
